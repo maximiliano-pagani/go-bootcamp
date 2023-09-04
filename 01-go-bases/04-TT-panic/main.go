969b45907bf069485f4561b98898825dc1cbc08c
maximiliano-pagani <maximiliano.pagani@mercadolibre.com>
2023-09-04 18:37:42 -0300

    feat: added 04-panic exercises

---
+++ 01-go-bases/04-TT-panic/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"04-panic/ex1"
+	"04-panic/ex2"
+	"04-panic/ex3"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Init 04-panic module")
+
+	fmt.Println("\nExercise 1\n ")
+	ex1.Ex1()
+
+	fmt.Println("\nExercise 2\n ")
+	ex2.Ex2()
+
+	fmt.Println("\nExercise 3\n ")
+	ex3.Ex3()
+}
