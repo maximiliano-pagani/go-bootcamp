5bfa9af262ad5c1a7688a8c77c01f9e3e697fa32
maximiliano-pagani <maximiliano.pagani@mercadolibre.com>
2023-09-03 23:15:58 -0300

    feat: interfaces exercises

---
+++ 01-go-bases/03-TT-interfaces/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"interfaces/ex1"
+	"interfaces/ex2"
+)
+
+func main() {
+	fmt.Println("Init interfaces exercises\n ")
+
+	fmt.Println("Exercise 1\n ")
+	Ex1()
+	fmt.Println()
+
+	fmt.Println("Exercise 2\n ")
+	Ex2()
+	fmt.Println()
+}
