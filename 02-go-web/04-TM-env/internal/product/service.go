8e9f1e637bf26dd457f4debd6662893a916c274d
maximiliano-pagani <maximiliano.pagani@mercadolibre.com>
2023-09-13 10:25:10 -0300

    feat: added 04-env exercises

---
+++ 02-go-web/04-TM-env/internal/product/service.go
@@ -0,0 +1,14 @@
+package product
+
+import "04-TM-env/internal/domain"
+
+type ProductService interface {
+	GetAllProducts() ([]domain.Product, error)
+	GetProductById(id int) (domain.Product, error)
+	GetProductByCode(code string) (domain.Product, error)
+	GetProductsByMinPrice(minPrice float64) ([]domain.Product, error)
+	NewProduct(product *domain.Product) (domain.Product, error)
+	ReplaceProduct(product *domain.Product) (domain.Product, error)
+	UpdateProduct(product *domain.Product) (domain.Product, error)
+	DeleteProduct(id int) error
+}
